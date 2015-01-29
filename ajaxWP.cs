e292958f2788822a6f4cad4016c72c997757d410
vs-admin <pankaj_jainani@infosys.com>
2015-01-29 11:47:33 +0530

    Create ajaxWP.cs
    Initial checkin

---
+++ ajaxWP.cs
@@ -0,0 +1,1 @@
+style="visibility: hidden; display: none; top: 104px; height: 56px; clip: rect(auto, auto, auto, auto); left: 290px; z-index: 1;"><table cellspacing="0" cellpadding="0" border="0" style="top: 0px;"><tbody><tr id="zz1_TopNavigationMenun4" onkeyup="Menu_Key(event)" onmouseout="Menu_Unhover(this)" onmouseover="Menu_HoverDynamic(this)"><td>
